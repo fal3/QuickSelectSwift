0fd8f3d355586c9724218c4cc4302c8f639ffa2f
Alex Fallah <fallaha@wit.edu>
2016-07-18 13:39:44 -0400

    Quick Select Algorithim implemented

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,52 @@
+//: Playground - noun: a place where people can play
+
+
+import UIKit
+
+func lomutoPartition<T: Comparable>(inout A: [T], left: Int, right: Int) -> Int {
+    let pivot = A[right]
+    
+    var s = left
+    for j in left..<right {
+        if A[j] <= pivot {
+            (A[s], A[j]) = (A[j], A[s])
+            s += 1
+        }
+    }
+    
+    (A[s], A[right]) = (A[right], A[s])
+    return s
+}
+/*
+ Swift's swap() doesn't like it if the items you're trying to swap refer to
+ the same memory location. This little wrapper simply ignores such swaps.
+ */
+public func swap<T>(inout a: [T], _ i: Int, _ j: Int) {
+    if i != j {
+        swap(&a[i], &a[j])
+    }
+}
+
+func quickSelect<T: Comparable>(inout a: [T], _ low: Int, _ high: Int, _ k: Int) -> T {
+    
+    if low < high {
+        let p = lomutoPartition(&a, left: low, right: high)
+        if k == p {
+            return a[p]
+        } else if k < p {
+            return quickSelect(&a, low, p - 1, k)  
+        } else {
+            return quickSelect(&a, p + 1, high, k)
+        }
+    } else {
+        return a[low]
+    }
+}
+
+var qSel = [ 10, 0, 3, 9, 2, 14, 26, 27, 1, 5, 8, -1, 8 ]
+
+var KAY = qSel.count/2
+
+//Input subarray[Left..Right], kth element, for the example we want the median, so we do qSel.count/2 th element
+quickSelect(&qSel, 0, qSel.count-1, KAY)
+
--- QuickSelectSwift/ViewController.swift
+++ QuickSelectSwift/ViewController.swift
@@ -12,13 +12,57 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var theList = [4,1,10,8,7,12,9,2,15]
+        
+        let example = lomutoPartition(&theList, left: 0, right: theList.count-1)
+        
     }
+    
+    func lomutoPartition<T: Comparable>(inout A: [T], left: Int, right: Int) -> Int {
+        let pivot = A[right]
+        
+        var s = left
+        for j in left..<right {
+            if A[j] <= pivot {
+                (A[s], A[j]) = (A[j], A[s])
+                s += 1
+            }
+        }
+        
+        (A[s], A[right]) = (A[right], A[s])
+        return s
+    }
+    /*
+     Swift's swap() doesn't like it if the items you're trying to swap refer to
+     the same memory location. This little wrapper simply ignores such swaps.
+     */
+    internal func swap<T>(inout A: [T], _ i: Int, _ j: Int) {
+        if i != j {
+            swap(&A, i, j)
+        }
+    }
+    
+    func quickSelect<T: Comparable>(inout A: [T], _ left: Int, _ right: Int, _ k: Int) -> T {
+        
+        if left < right {
+            let p = lomutoPartition(&A, left: left, right: right)
+            if k == p {
+                return A[p]
+            } else if k < p {
+                return quickSelect(&A, left, p - 1, k)
+            } else {
+                return quickSelect(&A, p + 1, right, k)
+            }
+        } else {
+            return A[left]
+        }
+    }
+    
+    
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+ 
 
 
 }
